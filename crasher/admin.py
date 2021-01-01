6ea65ac0466fb9b81efbd55ecd414f7002f77b99
bijaythapa <bijay.iwacademy@gmail.com>
2021-01-01 15:39:16 +0545

    [UPDATE]

--- crasher/admin.py
+++ crasher/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Post, Issue
 
-# Register your models here.
+
+class IssueInline(admin.TabularInline):
+    model = Issue
+
+
+class PostAdmin(admin.ModelAdmin):
+    inlines = [
+        IssueInline,
+    ]
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Issue)
