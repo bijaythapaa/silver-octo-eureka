ef7a874b774d0ce36fecf635e2746f653fa461df
bijaythapa <bijay.iwacademy@gmail.com>
2021-01-01 17:30:03 +0545

    [ADD] serializer file added

--- crasher/serializers.py
+++ crasher/serializers.py
@@ -4,7 +4,7 @@ from .models import Post, Issue
 
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
-        fields = ('id', 'author', 'title', 'body', 'created_at',)
+        fields = ('id', 'author', 'title', 'body', 'created_at', 'updated_at',)
         model = Post
 
 
