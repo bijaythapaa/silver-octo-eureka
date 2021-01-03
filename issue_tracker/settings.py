5651dd94955d76036124ca204965b16a11bba842
bijaythapa <bijay.iwacademy@gmail.com>
2021-01-03 12:22:45 +0545

    [delete] react app deleted

--- issue_tracker/settings.py
+++ issue_tracker/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
 
     # local apps
     'crasher.apps.CrasherConfig',
-    'frontend',
+
 ]
 
 
--- issue_tracker/urls.py
+++ issue_tracker/urls.py
@@ -20,5 +20,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('crasher.urls')),
     path('api-auth/', include('rest_framework.urls')),
-    path('', include('frontend.urls')),
 ]
